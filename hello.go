2337b1cfd672525837f52c6955e0f9d6fdd07c5e
sanchezc <xcarlossanchez@gmail.com>
2015-02-28 15:11:43 -0800

    Sanchez is in the game now

--- hello.go
+++ hello.go
@@ -12,8 +12,15 @@ func main(){
   fmt.Println("My name is " + firstName + " " + lastName)
   fmt.Println("I am " + IntToStr(age))
   fmt.Println("and I like tacos and burritos.")
-}
+  fmt.Println("")
 
+  lastname := "Sanchez"
+  fmt.Println("Hello, Ladies..")
+  fmt.Println("My name is " + firstName + " " + lastname)
+  fmt.Println("I am " + IntToStr(age))
+  fmt.Println("and I too, like tacos and burritos ")
+  fmt.Println("but I also enjoy piña coladas and long walks on the beach.")
+  }
 func IntToStr(i int) string {
   return strconv.FormatInt(int64(i),10)
 }
